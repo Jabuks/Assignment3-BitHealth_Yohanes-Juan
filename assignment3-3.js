d6e6abdaadd0435e67b18f3e6ad78c9d94fd8415
Jabuks <yohanes.juan@bithealth.co.id>
2024-01-16 14:45:17 +0700

    revisi assignment 3-3

--- assignment3-3.js
+++ assignment3-3.js
@@ -1,19 +1,36 @@
 function sumArray(arr, int) {
     var seen = {};
     var result = [];
-
-    for ( i = 0; i < arr.length; i++) {
+    // console.log(arr, "==> CURRENT VALUE");
+    // console.log(seen, "==> BEFORE");
+    for ( i = 0; i < arr.length; i++) { // sudah benar
      number = arr[i];
+
+    //  console.log(arr[i], "===> ARRAY INDEX ", i);
+
      complement = int - number;
-        if (seen[complement] !== undefined) {
+        // console.log(seen[complement], "==> SEEN");
+        let cekVariableTamp = [complement, number]
+        // console.log(cekVariableTamp, "==> BEFORRRR");
+        // console.log(seen[complement], "==> KEY OBJ");
+        if (seen[complement] === undefined) {  
+            // console.log(cekVariableTamp,"==> INDEX FINAL RESULT");
             result.push([complement, number]);
         }
         seen[number] = true;
+        // console.log(complement, "==> complement");
+        // console.log(number, "=> number");
     }
+
+    // console.log(seen, "==> AFTER");
 
     return result;
 }
 
-// test
 console.log(sumArray([2, 1, 4, 3], 5)); //  [[2, 3], [1, 4]]
 console.log(sumArray([1, 8, 10, 3], 11)); //  [[1, 10], [8, 3]]
+
+
+const seen={
+    
+}
